7faf853474b87a2b181b028a54c52348cad4c242
Divyansh Verma <69747865+DivyanshVerma217@users.noreply.github.com>
2021-07-16 21:19:46 +0530

    Add files via upload

---
+++ Response.js
@@ -0,0 +1,369 @@
+export default {
+    "kind": "customsearch#search",
+    "url": {
+        "type": "application/json",
+        "template": "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json"
+    },
+    "queries": {
+        "request": [
+            {
+                "title": "Google Custom Search - h",
+                "totalResults": "439000000",
+                "searchTerms": "h",
+                "count": 10,
+                "startIndex": 1,
+                "inputEncoding": "utf8",
+                "outputEncoding": "utf8",
+                "safe": "off",
+                "cx": "85016dbc2ebdfa595"
+            }
+        ],
+        "nextPage": [
+            {
+                "title": "Google Custom Search - h",
+                "totalResults": "439000000",
+                "searchTerms": "h",
+                "count": 10,
+                "startIndex": 11,
+                "inputEncoding": "utf8",
+                "outputEncoding": "utf8",
+                "safe": "off",
+                "cx": "85016dbc2ebdfa595"
+            }
+        ]
+    },
+    "context": {
+        "title": "Google"
+    },
+    "searchInformation": {
+        "searchTime": 0.604509,
+        "formattedSearchTime": "0.60",
+        "totalResults": "439000000",
+        "formattedTotalResults": "439,000,000"
+    },
+    "items": [
+        {
+            "kind": "customsearch#result",
+            "title": "H Mart - Google Maps",
+            "htmlTitle": "<b>H</b> Mart - Google Maps",
+            "link": "https://www.google.com/maps/search/?api=1&query=H+Mart&query_place_id=ChIJNVBZj1gtW4YRikK4GA_sSTw",
+            "displayLink": "www.google.com",
+            "snippet": "H Mart. Connect to internet to see place info.",
+            "htmlSnippet": "<b>H</b> Mart. Connect to internet to see place info.",
+            "cacheId": "dHus2EZmcIkJ",
+            "formattedUrl": "https://www.google.com/maps/search/?api=1&query=H+Mart...id...",
+            "htmlFormattedUrl": "https://www.google.com/maps/search/?api=1&amp;query=<b>H</b>+Mart...id...",
+            "pagemap": {
+                "cse_thumbnail": [
+                    {
+                        "src": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQUDTMfgWSotdHvbOKW4AmY1steqsilnDJS4q33zGx-d0AXvvxPiFY9Mvs",
+                        "width": "204",
+                        "height": "247"
+                    }
+                ],
+                "metatags": [
+                    {
+                        "referrer": "origin",
+                        "twitter:card": "summary",
+                        "theme-color": "#3367d6",
+                        "viewport": "initial-scale=1.0, user-scalable=no",
+                        "google": "notranslate"
+                    }
+                ],
+                "cse_image": [
+                    {
+                        "src": "https://maps.gstatic.com/tactile/basepage/pegman_sherlock.png"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Gmail",
+            "htmlTitle": "Gmail",
+            "link": "https://www.google.com/gmail/",
+            "displayLink": "www.google.com",
+            "snippet": "Gmail is email that's intuitive, efficient, and useful. 15 GB of storage, less spam, and mobile access.",
+            "htmlSnippet": "Gmail is email that&#39;s intuitive, efficient, and useful. 15 GB of storage, less spam, and mobile access.",
+            "formattedUrl": "https://www.google.com/gmail/",
+            "htmlFormattedUrl": "https://www.google.com/gmail/",
+            "pagemap": {
+                "metatags": [
+                    {
+                        "viewport": "width=device-width, initial-scale=1",
+                        "format-detection": "telephone=no"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Google Maps",
+            "htmlTitle": "Google Maps",
+            "link": "https://www.google.com/maps/contrib/106041631828084355675/reviews",
+            "displayLink": "www.google.com",
+            "snippet": "ya H. Local Guide · Level 5. 0 points1 point 1,278 points points points points points points. 500. 1,500. Looking for your own reviews and photos? You can ...",
+            "htmlSnippet": "ya <b>H</b>. Local Guide · Level 5. 0 points1 point 1,278 points points points points points points. 500. 1,500. Looking for your own reviews and photos? You can&nbsp;...",
+            "cacheId": "JzKq28ZGGRcJ",
+            "formattedUrl": "https://www.google.com/maps/contrib/106041631828084355675/reviews",
+            "htmlFormattedUrl": "https://www.google.com/maps/contrib/106041631828084355675/reviews",
+            "pagemap": {
+                "cse_thumbnail": [
+                    {
+                        "src": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS6AKPxcKCbFW7eGHGdTjCiqmdQQ5IgvK--21nWN8kIjF239s-qTIZ3Jk8T",
+                        "width": "310",
+                        "height": "163"
+                    }
+                ],
+                "metatags": [
+                    {
+                        "referrer": "origin",
+                        "og:image": "https://maps.gstatic.com/tactile/geoprofile/sharing/lg_5.png",
+                        "og:image:width": "256",
+                        "twitter:card": "summary",
+                        "og:site_name": "Contributions by ya H",
+                        "viewport": "initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no",
+                        "og:title": "Contributions by ya H",
+                        "og:image:height": "256",
+                        "google": "notranslate",
+                        "og:description": "Level 5 Local Guide | 1,278 Points"
+                    }
+                ],
+                "place": [
+                    {
+                        "image": "https://maps.gstatic.com/tactile/geoprofile/sharing/lg_5.png",
+                        "name": "Contributions by ya H",
+                        "description": "Level 5 Local Guide | 1,278 Points"
+                    }
+                ],
+                "cse_image": [
+                    {
+                        "src": "https://maps.gstatic.com/tactile/geoprofile/sharing/lg_5.png"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Google Maps - Stephanie Gibson",
+            "htmlTitle": "Google Maps - Stephanie Gibson",
+            "link": "https://www.google.com/maps/contrib/105043124304622667673/reviews",
+            "displayLink": "www.google.com",
+            "snippet": "Stephanie Gibson. 2 contributions. Looking for your own reviews and photos? You can contribute on Google Maps too! See my profile. Reviews Photos ...",
+            "htmlSnippet": "Stephanie Gibson. 2 contributions. Looking for your own reviews and photos? You can contribute on Google Maps too! See my profile. Reviews Photos&nbsp;...",
+            "cacheId": "mMNqdEtvVQoJ",
+            "formattedUrl": "https://www.google.com/maps/contrib/105043124304622667673/reviews",
+            "htmlFormattedUrl": "https://www.google.com/maps/contrib/105043124304622667673/reviews",
+            "pagemap": {
+                "cse_thumbnail": [
+                    {
+                        "src": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSjsuMXw41HvKt-V5343CUF8T9SctSxlub6gOjFnmpdNLm0us0e5clNujI",
+                        "width": "310",
+                        "height": "163"
+                    }
+                ],
+                "metatags": [
+                    {
+                        "referrer": "origin",
+                        "og:image": "https://maps.gstatic.com/tactile/geoprofile/sharing/non_lg.png",
+                        "og:image:width": "256",
+                        "twitter:card": "summary",
+                        "og:site_name": "Contributions by Stephanie Gibson",
+                        "viewport": "initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no",
+                        "og:title": "Contributions by Stephanie Gibson",
+                        "og:image:height": "256",
+                        "google": "notranslate",
+                        "og:description": "32 Contributions"
+                    }
+                ],
+                "place": [
+                    {
+                        "image": "https://maps.gstatic.com/tactile/geoprofile/sharing/non_lg.png",
+                        "name": "Contributions by Stephanie Gibson",
+                        "description": "32 Contributions"
+                    }
+                ],
+                "cse_image": [
+                    {
+                        "src": "https://maps.gstatic.com/tactile/geoprofile/sharing/non_lg.png"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Google Slides: Free Online Presentations for Personal Use",
+            "htmlTitle": "Google Slides: Free Online Presentations for Personal Use",
+            "link": "https://www.google.com/slides/about/",
+            "displayLink": "www.google.com",
+            "snippet": "Create a new presentation and edit it with others at the same time — from your computer, phone or tablet. Free with a Google account.",
+            "htmlSnippet": "Create a new presentation and edit it with others at the same time — from your computer, phone or tablet. Free with a Google account.",
+            "cacheId": "BqbRM4Tdrv4J",
+            "formattedUrl": "https://www.google.com/slides/about/",
+            "htmlFormattedUrl": "https://www.google.com/slides/about/",
+            "pagemap": {
+                "cse_thumbnail": [
+                    {
+                        "src": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS1kSln2ksdd2AV8fXzGqs682zFsdIBlpuwsLDEBCnNM7p92Fvd6clBNQ",
+                        "width": "120",
+                        "height": "120"
+                    }
+                ],
+                "metatags": [
+                    {
+                        "viewport": "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
+                    }
+                ],
+                "cse_image": [
+                    {
+                        "src": "https://www.google.com/slides/about/images/common/add-tom.jpg"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Google Docs: Free Online Documents for Personal Use",
+            "htmlTitle": "Google Docs: Free Online Documents for Personal Use",
+            "link": "https://www.google.com/docs/about/",
+            "displayLink": "www.google.com",
+            "snippet": "Create a new document and edit it with others at the same time — from your computer, phone or tablet. Free with a Google account.",
+            "htmlSnippet": "Create a new document and edit it with others at the same time — from your computer, phone or tablet. Free with a Google account.",
+            "cacheId": "B4Skq4M5akkJ",
+            "formattedUrl": "https://www.google.com/docs/about/",
+            "htmlFormattedUrl": "https://www.google.com/docs/about/",
+            "pagemap": {
+                "cse_thumbnail": [
+                    {
+                        "src": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRmfEVwa5OKVco_cFYnxXB3H4y0t_LwidtQwOBqkrPjAZARR33gkHHd",
+                        "width": "120",
+                        "height": "120"
+                    }
+                ],
+                "metatags": [
+                    {
+                        "viewport": "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
+                    }
+                ],
+                "cse_image": [
+                    {
+                        "src": "https://www.google.com/docs/about/images/common/add-tom.jpg"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Noto CJK – Google Noto Fonts",
+            "htmlTitle": "Noto CJK – Google Noto Fonts",
+            "link": "https://www.google.com/get/noto/help/cjk/",
+            "displayLink": "www.google.com",
+            "snippet": "Noto Sans CJK and Noto Serif CJK comprehensively cover Simplified Chinese, Traditional Chinese, Japanese, and Korean in a unified font family. This includes  ...",
+            "htmlSnippet": "Noto Sans CJK and Noto Serif CJK comprehensively cover Simplified Chinese, Traditional Chinese, Japanese, and Korean in a unified font family. This includes &nbsp;...",
+            "cacheId": "ZioWH604oFUJ",
+            "formattedUrl": "https://www.google.com/get/noto/help/cjk/",
+            "htmlFormattedUrl": "https://www.google.com/get/noto/help/cjk/",
+            "pagemap": {
+                "metatags": [
+                    {
+                        "referrer": "no-referrer",
+                        "viewport": "initial-scale=1, minimum-scale=1, width=device-width"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Google Sheets: Free Online Spreadsheets for Personal Use",
+            "htmlTitle": "Google Sheets: Free Online Spreadsheets for Personal Use",
+            "link": "https://www.google.com/sheets/about/",
+            "displayLink": "www.google.com",
+            "snippet": "Create a new spreadsheet and edit it with others at the same time — from your computer, phone or tablet. Free with a Google account.",
+            "htmlSnippet": "Create a new spreadsheet and edit it with others at the same time — from your computer, phone or tablet. Free with a Google account.",
+            "cacheId": "95sduKEJz2gJ",
+            "formattedUrl": "https://www.google.com/sheets/about/",
+            "htmlFormattedUrl": "https://www.google.com/sheets/about/",
+            "pagemap": {
+                "cse_thumbnail": [
+                    {
+                        "src": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuWXylypSOE0-CB12oKInsNLeDhrvi2gRJXR7LcU4EXM20e-yi6-OH",
+                        "width": "120",
+                        "height": "120"
+                    }
+                ],
+                "metatags": [
+                    {
+                        "viewport": "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
+                    }
+                ],
+                "cse_image": [
+                    {
+                        "src": "https://www.google.com/sheets/about/images/common/add-kim.jpg"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Google Earth",
+            "htmlTitle": "Google Earth",
+            "link": "https://www.google.com/earth/index.html",
+            "displayLink": "www.google.com",
+            "snippet": "Create stories and maps. With creation tools, you can draw on the map, add your photos and videos, customize your view, and share and collaborate with others ...",
+            "htmlSnippet": "Create stories and maps. With creation tools, you can draw on the map, add your photos and videos, customize your view, and share and collaborate with others&nbsp;...",
+            "cacheId": "hG4K3PHoZqAJ",
+            "formattedUrl": "https://www.google.com/earth/index.html",
+            "htmlFormattedUrl": "https://www.google.com/earth/index.html",
+            "pagemap": {
+                "cse_thumbnail": [
+                    {
+                        "src": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT2iTbFc46zcN_-4vw73SSiOqqud55-3a81RiHCSkbw7pyXozBNG-RUggxs",
+                        "width": "282",
+                        "height": "179"
+                    }
+                ],
+                "metatags": [
+                    {
+                        "og:image": "https://gweb-earth.storage.googleapis.com/assets/google_earth_banner.jpg",
+                        "og:type": "website",
+                        "og:site_name": "Google Earth",
+                        "viewport": "width=device-width, initial-scale=1.0, maximum-scale=2.0, height=device-height",
+                        "og:title": "Overview – Google Earth",
+                        "og:url": "https://www.google.com/earth/"
+                    }
+                ],
+                "cse_image": [
+                    {
+                        "src": "https://www.google.com/intl/en_us/earth/assets/static/images/studio_video_mobile_poster.png"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Google Sky",
+            "htmlTitle": "Google Sky",
+            "link": "https://www.google.com/sky/",
+            "displayLink": "www.google.com",
+            "snippet": "Solar System · Constellations · Hubble Showcase · Backyard Astronomy · Chandra X-Ray Showcase · GALEX Ultraviolet Showcase · Spitzer Infrared Showcase · Earth ...",
+            "htmlSnippet": "Solar System &middot; Constellations &middot; Hubble Showcase &middot; Backyard Astronomy &middot; Chandra X-Ray Showcase &middot; GALEX Ultraviolet Showcase &middot; Spitzer Infrared Showcase &middot; Earth&nbsp;...",
+            "cacheId": "M07vsy8M2X0J",
+            "formattedUrl": "https://www.google.com/sky/",
+            "htmlFormattedUrl": "https://www.google.com/sky/",
+            "pagemap": {
+                "cse_thumbnail": [
+                    {
+                        "src": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsr9eTIxtYnl1X1bYkUfcbsVQwtHVPzfMLoZbhUAzlHuuS-MUFPzoLFAk",
+                        "width": "225",
+                        "height": "225"
+                    }
+                ],
+                "cse_image": [
+                    {
+                        "src": "https://mw1.google.com/mw-planetary/sky/skytiles_v1/75_27_7.jpg"
+                    }
+                ]
+            }
+        }
+    ]
+}
